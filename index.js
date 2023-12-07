2393459d00cf406ec005dcdbfbcae574acce0db9
Urian Sanchez <urain@Urians-MacBook-Pro.local>
2023-12-07 14:37:37 -0700

    Adding the files from the workshop

---
+++ index.js
@@ -0,0 +1,108 @@
+// === DO NOT EDIT THIS REGION ===
+// Read the comments to understand how the program is structured.
+
+// Prompt the user for a list of integers separated by commas.
+const userInputString = prompt(
+  "Please enter some integers separated by commas.",
+  "1,2,3,4,5"
+);
+
+// Split the string of numbers into an array of strings.
+const stringArray = userInputString.split(",");
+
+// Convert the array of strings into an array of numbers.
+const numbers = [];
+for (let i = 0; i < stringArray.length; i++) {
+  const str = stringArray[i];
+  const number = parseInt(str);
+  numbers.push(number);
+}
+
+// === EDIT THE CODE BELOW ===
+// Complete the functions below to make the program work!
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the length of the array
+ */
+
+const getLength = (numbers) => {
+  return numbers.length;
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the sum of the numbers
+ */
+
+const getSum = (numbers) => {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+    sum = sum + numbers[i];
+  }
+  return sum;
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the mean of the numbers
+ */
+
+const getMean = (numbers) => {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  return getSum(numbers) / numbers.length;
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the smallest of the numbers
+ */
+const getMin = (numbers) => {
+  return Math.min(...numbers);
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the largest of the numbers
+ */
+const getMax = (numbers) => {
+  return Math.max(...numbers);
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number} the range of the numbers (max - min)
+ */
+const getRange = (numbers) => {
+  return getMax(numbers) - getMin(numbers);
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number[]} the even numbers in the array
+ */
+const getEvens = (numbers) => {
+  return numbers.filter((num) => num % 2 == 0);
+};
+
+/**
+ * @param {number[]} numbers an array of integers
+ * @returns {number[]} the odd numbers in the array
+ */
+const getOdds = (numbers) => {
+  return numbers.filter((num) => num % 2 !== 0);
+};
+
+// Peform some calculations on the numbers.
+console.log(numbers);
+console.log(`You have given ${getLength(numbers)} numbers.`);
+console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+console.log(`The range of your numbers is ${getRange(numbers)}.`);
+console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
